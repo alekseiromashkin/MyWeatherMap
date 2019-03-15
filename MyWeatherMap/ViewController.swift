312b07aba0679f7978d78234709ff1c7e061dfb5
Aleksei Romashkin <alekseiromashkin@Alekseis-MacBook-Pro.local>
2019-03-15 16:23:57 +0300

    Initial Commit

---
+++ MyWeatherMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeatherMap
+//
+//  Created by Aleksei Romashkin on 15/03/2019.
+//  Copyright © 2019 Kyzylasker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
